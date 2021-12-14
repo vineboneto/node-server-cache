5256ea4782a38421cf86c6f1d687dff1847daa74
vineboneto <vineboneto@gmail.com>
2021-12-14 11:14:13 -0300

    feat: initial commit

---
+++ prisma/seed.ts
@@ -0,0 +1,21 @@
+import { PrismaClient, User } from '@prisma/client'
+import faker from 'faker'
+
+const client = new PrismaClient()
+
+type UserRandom = Omit<User, 'id' | 'created_at'>
+
+const generateRandomUsers = (): UserRandom[] => {
+  const data: UserRandom[] = []
+  for (let i = 0; i < 1000000; i++) {
+    data.push({ email: faker.internet.email(), password: faker.internet.password(), username: faker.name.findName() })
+  }
+  return data
+}
+
+client.user
+  .createMany({
+    data: generateRandomUsers(),
+  })
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err))
---
+++ readme.md
@@ -0,0 +1,23 @@
+# About
+
+Studying backend, populating any table with 1,000,000,000 of data
+
+- Caching
+
+## Requirements
+
+- Postgres >= 13.x
+- NodeJS >= 14.x
+
+## Installation
+
+```shell
+# Install dependencies
+$ yarn
+
+# Generate cliente prisma
+$ yarn gen
+
+# Generate seeds
+$ yarn seed
+```
